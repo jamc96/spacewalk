f4199f43bf591d999ba6fff02b6c2b8e5ba0940f
jamc96 <jamc.mejia@gmail.com>
2018-11-01 07:22:21 -0600

    updating main unit testing to check the yumrepos

--- spec/classes/spacewalk_spec.rb
+++ spec/classes/spacewalk_spec.rb
@@ -16,38 +16,20 @@ describe 'spacewalk' do
       let :params do
         { channels: ["#{os}-channel1", "#{os}-channel2"] }
       end
-
-      # default variables
-      package_url = "https://copr-be.cloud.fedoraproject.org/archive/spacewalk/2.6-client/RHEL/#{os_facts[:operatingsystemmajrelease]}/x86_64"
-      epel_key = "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-#{os_facts[:operatingsystemmajrelease]}"
       # compilation errors
       it { is_expected.to compile }
       it { is_expected.to compile.with_all_deps }
       # keys
-      it { is_expected.to contain_file(epel_key).with(ensure: 'present', mode: '0644') }
+      ["EPEL-#{os_facts[:operatingsystemmajrelease]}", 'Spacewalk-client', 'Spacewalk-nightly'].each do |key|
+        it { is_expected.to contain_file("/etc/pki/rpm-gpg/RPM-GPG-KEY-#{key}").with(ensure: 'present', mode: '0644') }
+      end
       # yumrepo
-      it {
-        is_expected.to contain_yumrepo('epel').with(
-          ensure: 'present',
-          descr: "Extra Packages for Enterprise Linux #{os_facts[:operatingsystemmajrelease]} - \$basearch",
-          enabled: '1',
-          failovermethod: 'priority',
-          gpgcheck: '1',
-          gpgkey: "file://#{epel_key}",
-          mirrorlist: "https://mirrors.fedoraproject.org/metalink?repo=epel-#{os_facts[:operatingsystemmajrelease]}&arch=\$basearch",
-          require: "File[#{epel_key}]",
-        )
-      }
+      ['epel', 'spacewalk-client', 'spacewalk-client-nightly'].each do |key|
+        it { is_expected.to contain_yumrepo(key).with_ensure('present') }
+      end
       # packages
-      it {
-        is_expected.to contain_package('spacewalk-client-repo').with(
-          ensure: 'present',
-          provider: 'rpm',
-          source: "#{package_url}/spacewalk-client-repo-2.6-0.el#{os_facts[:operatingsystemmajrelease]}.noarch.rpm"
-        )
-      }
       ['yum-rhn-plugin', 'rhn-setup'].each do |key|
-        it { is_expected.to contain_package(key).with(ensure: 'present', provider: 'yum', require: 'Package[spacewalk-client-repo]') }
+        it { is_expected.to contain_package(key).with(ensure: 'present', provider: 'yum', require: 'Yumrepo[spacewalk-client]') }
       end
       ['python-dmidecode', 'python-hwdata'].each do |key|
         it { is_expected.to contain_package(key).with(ensure: 'present', provider: 'yum', require: 'Yumrepo[epel]') }
